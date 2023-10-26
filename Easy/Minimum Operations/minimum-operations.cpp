66c38c87a8271911f545a7851c8ba8e4296245da
mohit-8692 <132361060+mohit-8692@users.noreply.github.com>
2023-10-26 20:00:43 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Minimum Operations/minimum-operations.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+  public:
+    int minOperation(int n)
+    {
+        //code here.
+        int cnt = 0;
+        if(n==0)return cnt;
+        
+        while(n!=0)
+        {
+            if(n%2 ==0)
+            {
+                n=n/2;
+                cnt++;
+            }
+            else{
+                n=n-1;
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	    {
+	        int n;
+	        cin>>n;
+	        Solution ob;
+	        cout<<ob.minOperation(n)<<endl;
+	    }
+}
+// } Driver Code Ends
