39afa2985dc8c354a4f7f9382bf655a6a23de417
mohit-8692 <132361060+mohit-8692@users.noreply.github.com>
2023-11-28 17:46:47 +0530

    Updated Solution - GfG to GitHub

--- Medium/Shuffle integers/shuffle-integers.cpp
+++ Medium/Shuffle integers/shuffle-integers.cpp
@@ -9,42 +9,20 @@ class Solution{
 	
 	public:
 	void shuffleArray(int arr[],int n)
-	{
+	{   int k = (n/2)-1;
 	    // Your code goes here
-	   // int temp1 = 0;
-	   // int temp2 = 0;
-	   // int j = n/2;
-	   // int i =1;
-	    
-	   // for(int i = 1; i<n; i = i+2)
-	   // {
-	   //     temp1 = arr[i];
-	   //     temp2 = arr[i+2];
-	   //      arr[i+1] = temp1;
-	   //     arr[i] =arr[j];
-	   //     j++;
-	   // }
-	   
-	   vector<int>ans1;
-	   vector<int>ans2;
-	       int j = 0;
-	   for(int i = n/2; i<n; i++)
-	   {
-	       ans1.push_back(arr[j]);
-	       ans2.push_back(arr[i]);
-	       j++;
-	   }
-	       int k =0;
-	        j= 0;
-	       for(int i = 0; i<n; i++)
-	   {
-	        if(i%2 == 0)
-	            {arr[i] = ans1[j];
-	            j++;}
-	        else
-	            {arr[i] = ans2[k];
-	            k++;}
-	   }
+	    for(int i = n-1; i >=n/2; i--)
+	    {
+	        arr[i] <<= 10;
+	        arr[i] |= arr[k];
+	        k--;
+	    }
+	    k = 0;
+	    for(int i =n/2; i<n; i++)
+	    {
+	        arr[k++] = arr[i] & 1023;
+	        arr[k++] = arr[i] >> 10;
+	    }
 	}
 		 
 
