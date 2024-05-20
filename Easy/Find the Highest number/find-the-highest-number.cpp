52acbc08445d4ef97761899439dfd5b145dffd47
Mohit Gupta <132361060+mohit-8692@users.noreply.github.com>
2024-05-20 15:08:53 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Find the Highest number/find-the-highest-number.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+public:
+    int findPeakElement(vector<int>& a) 
+    {
+        // Code here.
+        int s = 0, l = a.size()-1; 
+        int ans = 0;
+         int mid = (s+l)/2;
+        while(s<l)
+        {
+            mid = (s+l)/2;
+            if((a[mid] > a[mid+1]))
+                l = mid;
+            else
+                s = mid+1;
+        }
+        return a[s];
+    }
+};
+
+
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n;
+    	cin >> n;
+    	vector<int>a(n);
+    	for(int i = 0; i < n; i++)
+    		cin>>a[i];
+    	Solution ob;
+    	int ans = ob.findPeakElement(a);
+    	cout << ans << "\n";
+    }
+	return 0;
+}
+
+
+// } Driver Code Ends
